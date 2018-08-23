945e1af07e03b7c3cbe3ed41e450da4e11e75a6b
Reinhartist <jordanventers@gmail.com>
2018-08-23 13:24:50 +0100

    Fixed sonar suggestion

--- src/main/java/com/qa/account_application/App.java
+++ src/main/java/com/qa/account_application/App.java
@@ -1,6 +1,7 @@
 package com.qa.account_application;
 
 public class App {
+	private App() {}
     public static void main(String[] args) {
         System.out.println("Hello world to the standard out");
     }
